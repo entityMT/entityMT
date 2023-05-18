6be7e633e34a6819781de4ca227b042031e73fd6
gustavo.paggiatto <gustavo.paggiatto@softplan.com.br>
2023-05-17 22:52:24 -0300

    Add Ghost Attribute (use at queries assemblies).

---
+++ MTConfigurations/MTConfigurations.Abstractions/Attributes/GhostAttribute.cs
@@ -0,0 +1,8 @@
+namespace MTConfigurations.Abstractions.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public sealed class GhostAttribute : Attribute
+    {
+
+    }
+}
